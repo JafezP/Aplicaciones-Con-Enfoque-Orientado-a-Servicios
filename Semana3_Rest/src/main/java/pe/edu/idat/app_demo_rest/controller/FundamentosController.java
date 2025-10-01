91fc506f33c5f2597ed0d671557efd9748dfaf92
Jafezp <fernandezp.joe@gmail.com>
2025-09-30 22:03:51 -0500

    Semana 3 Rest - COMPLETO

---
+++ Semana3_Rest/src/main/java/pe/edu/idat/app_demo_rest/controller/FundamentosController.java
@@ -0,0 +1,52 @@
+package pe.edu.idat.app_demo_rest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.idat.app_demo_rest.dto.FactorialDto;
+import pe.edu.idat.app_demo_rest.dto.FibonacciDto;
+import pe.edu.idat.app_demo_rest.dto.PrimoDto;
+import pe.edu.idat.app_demo_rest.service.FundamentosService;
+
+@RestController
+@RequestMapping("/api/v1/operaciones")
+public class FundamentosController {
+
+    private final FundamentosService fundamentosService;
+
+    public FundamentosController(FundamentosService fundamentosService) {
+        this.fundamentosService = fundamentosService;
+    }
+
+    //localhost:8055/api/v1/operaciones/primo?=numero=5
+    @GetMapping("/primo")
+    public ResponseEntity<PrimoDto> validarNumeroPrimo(@RequestParam Integer numero) {
+        PrimoDto primoDto = new PrimoDto();
+
+        primoDto.setNumero(numero);
+        primoDto.setResultado(fundamentosService.validarNumeroPrimo(numero));
+
+        return ResponseEntity.ok(primoDto);
+    }
+
+    //localhost:8055/api/v1/operaciones/factorial?=numero=5
+    @GetMapping("/factorial")
+    public ResponseEntity<FactorialDto> validarFactorial(@RequestParam(required = false, defaultValue = "2") Integer numero) {
+        FactorialDto factorialDto = new FactorialDto();
+
+        factorialDto.setNumero(numero);
+        factorialDto.setValorFactorial(fundamentosService.calcularFactorial(numero));
+
+        return ResponseEntity.ok(factorialDto);
+    }
+
+    @GetMapping({"/fibonacci", "/fibonacci/{numero}"})
+    public ResponseEntity<FibonacciDto> calcularFibonacci(@PathVariable(required = false) Integer numero) {
+        if (numero == null) numero = 1;
+        FibonacciDto fibonacciDto = new FibonacciDto();
+
+        fibonacciDto.setCantidadNumero(numero);
+        fibonacciDto.setFibonacci(fundamentosService.calcularFibonacci(numero));
+
+        return ResponseEntity.ok(fibonacciDto);
+    }
+}
--- Semana3_Rest/src/main/java/pe/edu/idat/app_demo_rest/dto/FibonacciDto.java
+++ Semana3_Rest/src/main/java/pe/edu/idat/app_demo_rest/dto/FibonacciDto.java
@@ -1,4 +1,25 @@
 package pe.edu.idat.app_demo_rest.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonacciDto {
+    private Integer cantidadNumero;
+    private List<Long> fibonacci = new ArrayList<>();
+
+    public Integer getCantidadNumero() {
+        return cantidadNumero;
+    }
+
+    public void setCantidadNumero(Integer cantidadNumero) {
+        this.cantidadNumero = cantidadNumero;
+    }
+
+    public List<Long> getFibonacci() {
+        return fibonacci;
+    }
+
+    public void setFibonacci(List<Long> fibonacci) {
+        this.fibonacci = fibonacci;
+    }
 }
